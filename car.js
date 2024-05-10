95bd0ee4cda172e92bda5ea40f0809ab76410801
Luna Aljammal <aljammal@mcmaster.ca>
2024-05-09 23:48:46 -0400

    up/down motion of car activated by key controls

--- car.js
+++ car.js
@@ -5,8 +5,42 @@ class Car{
         this.width = width;
         this.height = height;
 
+        this.speed = 0;
+        this.acceleration = 0.3;
+        this.speedLimit = 4;
+        this.friction = 0.04;
         this.controller = new Controller();
     }
+
+    update(){
+        if(this.controller.forward){
+            this.speed+=this.acceleration;
+        }
+        if(this.controller.reverse){
+            this.speed-=this.acceleration;
+        }
+
+        if(this.speed>this.speedLimit){
+            this.speed=this.speedLimit;
+        }
+        if(this.speed<-this.speedLimit/2){
+            this.speed=-this.speedLimit/2;
+        }
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+        
+
+        this.y-=this.speed;
+    }            
+
+
     draw(ctx){
         ctx.beginPath();
         ctx.rect(
--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 //creating road on canvas
 const canvas = document.getElementById('startingCanvas');
-canvas.height=window.innerHeight;
 canvas.width=200;
 
 //creating car on canvas
 const ctx = canvas.getContext("2d");
 const car=new Car(100,100,25,45);
-car.draw(ctx);
+
+
+animate();
+
+function animate() {
+    car.update();
+    canvas.height=window.innerHeight;     car.draw(ctx);
+    requestAnimationFrame(animate);
+    car.draw(ctx);
+
+}    
