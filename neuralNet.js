cd7a8f6c71b911f2874780d6f7bd647a30573c83
Luna Aljammal <aljammal@mcmaster.ca>
2024-05-12 23:33:56 -0400

    added multiple levels

--- neuralNet.js
+++ neuralNet.js
@@ -1,3 +1,21 @@
+class NeuralNetwork{
+    constructor(numNeurons){
+        this.levels = [];
+        for(let i=0;i<numNeurons.length;i++){
+            this.levels.push(new Level(
+                numNeurons[i], numNeurons[i+1]
+            ));
+        }
+    }
+
+    static feedForward(givenInputs, network){
+        let outputs = Level.feedForward(givenInputs, network.levels[0]);
+        for (let i=1;i<network.levels.length;i++){
+            outputs=Level.feedForward(outputs, network.levels[i]);
+        }
+        return outputs;
+    }
+}
 class Level{
     constructor(numInput, numOutput){
         this.inputs= new Array(numInput);
